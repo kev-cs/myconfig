3a277e377246e715c0d916542e465815141e56b6
Kevin <>
2021-12-31 13:37:42 -0500

    Update config files

--- scripts/create-symlinks.sh
+++ scripts/create-symlinks.sh
@@ -1,10 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+# A simple shell script to bootstrap some personal config files.
 
 SCRIPTNAME=$(basename "$0")
 echo "$SCRIPTNAME"
 
 BASEDIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")"/..)"
-DOT_FILES=$(find "$BASEDIR/home-dotfiles/" -name "\.*" -type f)
+DOT_FILES=$(find "$BASEDIR/dotfiles/" -name "\.*" -type f)
 CONFIG_FILES="$BASEDIR/.config"/*
 
 echo "> Creating symlinks for dotfiles"
@@ -12,22 +13,22 @@ for file in $DOT_FILES; do
   ln -sfi "$file" ~/"$(basename "$file")"
 done
 
-echo "> Creating symlinks for config files"
-mkdir -p ~/.config
-for file in $CONFIG_FILES; do
-  echo $file
-  BASENAME="$(basename "$file")"
-
-  if [ -L ~/.config/"$BASENAME" ]; then
-    rm ~/.config/"$BASENAME"
-  elif [ -d ~/.config/"$BASENAME" ]; then
-    mkdir ~/.config/"$BASENAME"_bak
-    mv ~/.config/"$BASENAME"/* ~/.config/"$BASENAME"_bak
-    rmdir ~/.config/"$BASENAME"
-  fi
-
-  ln -sfi "$file" ~/.config/"$BASENAME"
-done
+#echo "> Creating symlinks for config files"
+#mkdir -p ~/.config
+#for file in $CONFIG_FILES; do
+#  echo $file
+#  BASENAME="$(basename "$file")"
+#
+#  if [ -L ~/.config/"$BASENAME" ]; then
+#    rm ~/.config/"$BASENAME"
+#  elif [ -d ~/.config/"$BASENAME" ]; then
+#    mkdir ~/.config/"$BASENAME"_bak
+#    mv ~/.config/"$BASENAME"/* ~/.config/"$BASENAME"_bak
+#    rmdir ~/.config/"$BASENAME"
+#  fi
+#
+#  ln -sfi "$file" ~/.config/"$BASENAME"
+#done
 
 echo "> Symlinking this repo in home folder"
 ln -s "$BASEDIR" "$HOME"
